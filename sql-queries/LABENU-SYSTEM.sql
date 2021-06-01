66e63b72da47e824655c21faed40ea16b4396a3c
Fabiana Genila <fabianagenila@gmail.com>
2021-06-01 17:11:33 -0300

    Acréscimo de Auto Increment nas queries SQL

--- sql-queries/LABENU-SYSTEM.sql
+++ sql-queries/LABENU-SYSTEM.sql
@@ -1,6 +1,6 @@
 -- TABELA TURMA
 CREATE TABLE turma_table(
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(50) NOT NULL,
   periodo VARCHAR(50) NOT NULL,
   data_inicio DATE NOT NULL,
@@ -10,40 +10,40 @@ CREATE TABLE turma_table(
 
 -- TABELA PASSATEMPO
 CREATE TABLE passatempo_table(
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(50) NOT NULL
 );
 
 -- TABELA ESPECIALIDADE
 CREATE TABLE especialidade_table(
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(50) NOT NULL
 ); 
 
 -- TABELA ESTUDANTES
 CREATE TABLE estudantes_table(
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL UNIQUE,
   data_nasc DATE NOT NULL,
-  turma_id INT NOT NULL,
+  turma_id INT NOT NULL AUTO_INCREMENT,
   FOREIGN KEY (turma_id) REFERENCES turma_table(id)
 );
 
 -- TABELA DOCENTE
 CREATE TABLE docente_table(
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL UNIQUE,
   data_nasc DATE NOT NULL,
-  turma_id INT NOT NULL,
+  turma_id INT NOT NULL AUTO_INCREMENT,
   FOREIGN KEY (turma_id) REFERENCES turma_table(id)
 );
 
 -- TABELA ESTUDANTE_PASSATEMPO
 CREATE TABLE estudante_passatempo(
-  estudante_id INT NOT NULL,
-  passatempo_id INT NOT NULL,
+  estudante_id INT NOT NULL AUTO_INCREMENT,
+  passatempo_id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (estudante_id, passatempo_id), 
   FOREIGN KEY (estudante_id) REFERENCES estudantes_table(id),
   FOREIGN KEY (passatempo_id) REFERENCES passatempo_table(id)
@@ -51,8 +51,8 @@ CREATE TABLE estudante_passatempo(
 
 -- TABLE DOCENTE ESPECIALIDADE
 CREATE TABLE docente_especialidade_table(
-  docente_id INT NOT NULL,
-  especialidade_id INT NOT NULL,
+  docente_id INT NOT NULL AUTO_INCREMENT,
+  especialidade_id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (docente_id, especialidade_id),
   FOREIGN KEY (docente_id) REFERENCES docente_table(id),
   FOREIGN KEY (especialidade_id) REFERENCES especialidade_table(id)
